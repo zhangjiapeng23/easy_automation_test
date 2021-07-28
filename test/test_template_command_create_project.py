1b7a07e2c132e64c993f6cbad650dcc524838f81
jameszhang <18373230129@163.com>
2021-07-28 10:40:33 +0800

    1. add template create testcase.

---
+++ test/test_template_command_create_project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @author: James Zhang
+# @data  : 2021/7/28
+
+from src.easy_automation.core.template_command import TemplateCommand
+
+
+class TestTemplateCreateProject:
+
+    def test_create_project(self):
+        action = 'startApiProject'
+        project_name = 'test_project'
+        template = TemplateCommand(action, project_name)
+        template.handle()
