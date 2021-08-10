8a10ade22c9594d6efef4fc0325944572b5f5c86
jameszhang <18373230129@163.com>
2021-08-10 17:05:00 +0800

    update appium_mixin method supprot return type

--- src/easy_automation/contrib/appium_mixin.py
+++ src/easy_automation/contrib/appium_mixin.py
@@ -2,6 +2,8 @@
 # -*- encoding: utf-8 -*-
 # @author: James Zhang
 # @data  : 2021/8/5
+from typing import List
+
 from appium.webdriver.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException, InvalidElementStateException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -20,27 +22,27 @@ class AppiumMixin:
     EXPLICIT_WAIT = 8
     android_toast = elements.Xpath("//android.widget.Toast")
 
-    def __init__(self, driver: WebDriver):
-        self.driver = driver.find_element()
+    # def __init__(self, driver: WebDriver):
+    #     self.driver = driver.find_element()
 
     @after_error_hook
-    def find(self, element_selector, timeout=EXPLICIT_WAIT):
+    def find(self, element_selector, timeout=EXPLICIT_WAIT) -> WebElement:
         log.info(f'Find element {element_selector[1]}')
         return WebDriverWait(self.driver, timeout)\
             .until(EC.presence_of_element_located(element_selector))
 
     @after_error_hook
-    def finds(self, element_selector, timeout=EXPLICIT_WAIT):
+    def finds(self, element_selector, timeout=EXPLICIT_WAIT) -> List:
         log.info(f'Find elements {element_selector[1]}')
         return WebDriverWait(self.driver, timeout)\
             .until(EC.presence_of_all_elements_located(element_selector))
 
     @after_error_hook
-    def find_from_element(self, element: WebElement, element_selector):
+    def find_from_element(self, element: WebElement, element_selector) -> WebElement:
         return element.find_element(*element_selector)
 
     @after_error_hook
-    def finds_from_element(self, element: WebElement, element_selector):
+    def finds_from_element(self, element: WebElement, element_selector) -> List:
         return element.find_elements(*element_selector)
 
     @after_error_hook
@@ -81,7 +83,7 @@ class AppiumMixin:
         self.find(element_selector, timeout).send_keys(value)
 
     @after_error_hook
-    def scroll_up_find(self, element_selector, max_time=5):
+    def scroll_up_find(self, element_selector, max_time=5) -> WebElement:
         screen_size = self.driver.get_window_size()
         width = screen_size['width']
         height = screen_size['height']
@@ -98,7 +100,7 @@ class AppiumMixin:
         raise NoSuchElementException(element_selector)
 
     @after_error_hook
-    def scroll_down_find(self, element_selector, max_time=5):
+    def scroll_down_find(self, element_selector, max_time=5) -> WebElement:
         screen_size = self.driver.get_window_size()
         width = screen_size['width']
         height = screen_size['height']
@@ -115,7 +117,7 @@ class AppiumMixin:
         raise NoSuchElementException(element_selector)
 
     @after_error_hook
-    def swipe_element_left_find(self, element: WebElement, element_selector, max_time=5):
+    def swipe_element_left_find(self, element: WebElement, element_selector, max_time=5) -> WebElement:
         ele_location = element.location
         ele_size = element.size
         while max_time:
@@ -133,7 +135,7 @@ class AppiumMixin:
         raise NoSuchElementException(element_selector)
 
     @after_error_hook
-    def swipe_element_right_find(self, element: WebElement, element_selector, max_time=5):
+    def swipe_element_right_find(self, element: WebElement, element_selector, max_time=5) -> WebElement:
         ele_location = element.location
         ele_size = element.size
         while max_time:
@@ -150,16 +152,16 @@ class AppiumMixin:
                 max_time -= 1
         raise NoSuchElementException(element_selector)
 
-    def get_element_text(self, element_selector, timeout=EXPLICIT_WAIT):
+    def get_element_text(self, element_selector, timeout=EXPLICIT_WAIT) -> str:
         return self.find(element_selector, timeout).text
 
-    def get_elements_text(self, element_selector, timeout=EXPLICIT_WAIT, _slice=None):
+    def get_elements_text(self, element_selector, timeout=EXPLICIT_WAIT, _slice=None) -> List:
         elements = self.finds(element_selector,timeout)
         if _slice and isinstance(_slice, slice):
             elements = elements[_slice]
         return (element.text for element in elements)
 
-    def get_toast(self, timeout=EXPLICIT_WAIT):
+    def get_toast(self, timeout=EXPLICIT_WAIT) -> str:
         return self.get_element_text(self.android_toast, timeout)
 
     def screenshot(self):
