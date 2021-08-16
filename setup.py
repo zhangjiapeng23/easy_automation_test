2aa3b3ec200ca302290fa59acef9935ba42e949d
jameszhang <18373230129@163.com>
2021-08-16 18:27:30 +0800

    upgrade 0.0.4.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='easy-automation-test',
-    version='0.0.4.2',
+    version='0.0.4.3',
     author='jameszhang',
     author_email='18373230129@163.com',
     description='Easy to create a web, mobile or api automation test project',
