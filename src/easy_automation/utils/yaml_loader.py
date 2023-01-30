490db0dddc02717a302b3dc4ad2b75ec733dfae1
jiapeng zhang <18373230129@163.com>
2023-01-30 19:09:25 +0800

    1、修改yaml文件加载路径 2、增加多个yaml_loader对象，数据合并的方法

--- src/easy_automation/utils/yaml_loader.py
+++ src/easy_automation/utils/yaml_loader.py
@@ -14,10 +14,10 @@ class YamlLoader(metaclass=Singleton):
 
     def __init__(self, yaml_file: str):
         '''
-        yaml_file should only pass file name under testdata directory
+        yaml_file 需传递从app module下到testdata.yml的全路径，如 /am_api_test/testdata/pre_testdata.yml
         :param yaml_file:
         '''
-        yaml_file = os.path.join(find_project_root_dir(), 'testdata', yaml_file)
+        yaml_file = os.path.join(find_project_root_dir(), yaml_file)
         if not (yaml_file.endswith('.yaml') or yaml_file.endswith('.yml')):
             raise TypeError('Only support parse yaml format file.')
         if not os.path.isfile(yaml_file):
@@ -41,20 +41,41 @@ class YamlLoader(metaclass=Singleton):
     def get(self, args):
         return self._yaml_loader.get(args)
 
+    def merge_from(self, other_yaml_loader):
+        if not (isinstance(other_yaml_loader, YamlLoader)):
+            raise RuntimeError("merge object must be an instance of YamlLoader")
+
+        for other_key in other_yaml_loader._yaml_loader.keys():
+            if other_key not in self._yaml_loader.keys():
+                self._yaml_loader[other_key] = other_yaml_loader._yaml_loader.get(other_key)
+
+        self._data = FrozenJson(self._yaml_loader)
+
+    def merge_to(self, other_yaml_loader):
+        if not (isinstance(other_yaml_loader, YamlLoader)):
+            raise RuntimeError("merge object must be an instance of YamlLoader")
+
+        for other_key in other_yaml_loader._yaml_loader.keys():
+            self._yaml_loader[other_key] = other_yaml_loader._yaml_loader.get(other_key)
+
+        self._data = FrozenJson(self._yaml_loader)
+
     def __getitem__(self, args):
         return self._yaml_loader[args]
 
+    def __getattr__(self, item):
+        if hasattr(self._yaml_loader, item):
+            return getattr(self._yaml_loader, item)
 
-
+        raise AttributeError(f"{self.__class__.__name__} does not hava '{item}' attribute.")
 
 
 if __name__ == '__main__':
-    test = YamlLoader('account_test.yaml')
-    test2 = YamlLoader('account_test.yaml')
-    test3 = YamlLoader('account_test.yaml')
-    print(test is test2)
-    print(test2 is test3)
-    print(test.data.test_account[1].name)
-    print(test['test_account'][0]['name'])
-    print(test.get('test_account')[0].get('name'))
+    with open("D:\下载\pre_test.yaml") as f:
+        a = yaml.safe_load(f)
+        for i in a.keys():
+            print(i)
+        c = a.get("apiVersion")
+        print(c)
+        print(a)
 
