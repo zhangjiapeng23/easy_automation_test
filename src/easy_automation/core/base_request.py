35f4ff8afef5c4d1a4e3ccd2ccc7a0924ea0659e
jiapeng zhang <18373230129@163.com>
2023-01-30 19:17:35 +0800

    1、移除废弃的project_host设置方式

--- src/easy_automation/core/base_request.py
+++ src/easy_automation/core/base_request.py
@@ -5,23 +5,15 @@
 import requests
 
 from ..utils.custom_logging import Logs
-from ..utils.setting import setting
-from ..utils.exception import ProjectHostNotSet
 
 
 class RequestBase:
     log = Logs(__name__)
 
     def __init__(self):
-        try:
-            self._host = setting.PROJECT_HOST
-        except AttributeError:
-            raise ProjectHostNotSet
         self._session = requests.session()
 
     def send(self, method, url, *args, **kwargs):
-        if not url.startswith('http'):
-            url = ''.join((self._host, url))
         self.log.debug('{} {}'.format(method, url))
         return self._session.request(method, url, *args, **kwargs)
 
