22ce91028fa3ddc52915119b1d9ef1cd5de5c04e
jiapeng zhang <18373230129@163.com>
2024-03-11 19:05:52 +0800

    1、add Assert 快捷引入

--- src/easy_test.py
+++ src/easy_test.py
@@ -6,10 +6,12 @@ from easy_automation.core.plugin import context
 from easy_automation.utils.common import easy_parametrize
 from easy_automation.utils.common import TearDown
 from easy_automation.core.base_request import RequestBase
+from easy_automation.utils.common import Assert
 
 __all__ = [
     'easy_parametrize',
     'TearDown',
     'context',
-    'RequestBase'
+    'RequestBase',
+    'Assert'
 ]
